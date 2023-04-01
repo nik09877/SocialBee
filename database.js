df96541df0abed84f96fa997fc5a0e04fcc8bf00
Nikhil Kumar Mishra <nikhilkumarmishra120@gmail.com>
2023-04-01 18:32:47 +0530

    fixed deprication warning

--- database.js
+++ database.js
@@ -9,7 +9,8 @@ require('dotenv').config();
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set('useCreateIndex', true);
+
 class Database {
   constructor() {
     this.connect();
