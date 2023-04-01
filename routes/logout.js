0802f62db7a3716a7fec0aaa70105cea9f1c3c62
Nikhil Kumar Mishra <nikhilkumarmishra120@gmail.com>
2023-04-01 20:33:52 +0530

    fixed logout error

--- routes/logout.js
+++ routes/logout.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser")
-const bcrypt = require("bcrypt");
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const User = require('../schemas/UserSchema');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get("/", (req, res, next) => {
-    
-    if(req.session) {
-        req.session.destroy(() => {
-            res.redirect("/login");
-        })
-    }
-})
+router.get('/', (req, res, next) => {
+  if (req.session) {
+    // req.session.destroy(() => {
+    //     res.redirect("/login");
+    // })
+    req.session = null;
+    res.redirect('/login');
+  }
+});
 
 module.exports = router;
